72220e2ea607d35bd65cbcd7630b9d70dc312677
IrinaMuravyova <miomir@yandex.ru>
2024-01-27 12:41:28 +0800

    Views completed

--- Flappy Bird/GameView.swift
+++ Flappy Bird/GameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  GameView.swift
 //  Flappy Bird
 //
 //  Created by Irina Muravyeva on 27.01.2024.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GameView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    GameView()
 }
