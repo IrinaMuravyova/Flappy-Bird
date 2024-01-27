15d026488923a49fbecc34e94186b6593480029f
IrinaMuravyova <miomir@yandex.ru>
2024-01-27 11:43:42 +0800

    Initial Commit

---
+++ Flappy Bird/Flappy_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flappy_BirdApp.swift
+//  Flappy Bird
+//
+//  Created by Irina Muravyeva on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Flappy_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
